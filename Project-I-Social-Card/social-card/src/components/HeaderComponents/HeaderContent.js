d365ea8c3cd5fda737f64e56177515a12bf7e2ec
Aaron T <troskaaaron@gmail.com>
2019-05-29 20:08:11 -0500

    figured out components and structure. Working on CSS now.

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+import HeaderTitle from './HeaderTitle.js'
+
+const HeaderContent = () => {
+  return (
+    <div>
+      <HeaderTitle />
+      <p>Let's learn React by building simple interfaces with components. Don't try to overthink it, just keep it simple and have fun. Once you feel confortable using componentsyou are well on your way to mastering React.</p>
+    </div>
+  );
+};
+
+export default HeaderContent;
